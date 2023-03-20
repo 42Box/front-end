5ab428c74eafc9da46420f5eb87a18c3cad87133
darjeeling4g <darjeeling4g@gmail.com>
2023-03-20 20:04:46 +0900

    optimize image load

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react';
 import styles from '../Home.module.css';
 import Body from '../Body';
 
 function Home () {
+    function useImage(src) {
+        const [loaded, setLoaded] = useState(false);
+        const [error, setError] = useState(false);
+        useEffect(() => {
+            const img = new Image();
+            img.onload = () => {
+            setLoaded(true);
+            };
+            img.onerror = () => {
+            setError(true);
+            };
+            img.src = src;
+        }, [src]);
+        return { loaded, error };
+    }
+    const { loaded, error } = useImage(process.env.PUBLIC_URL + "/images/png/tree.png")
+    if (error) {
+        return <div>Error loading image</div>
+    }
     return (
-        <div className={styles.background}>
-            <Body />
+        <div>
+        {loaded ?
+            <div className={styles.background}>
+                <Body />
+            </div>
+        :
+            <div>
+                <img className={styles.imageSearching} src="/images/png/searching.png" />
+                <h1 className={styles.loading}>Loading...</h1>
+            </div>
+        }
         </div>
     );
 }
